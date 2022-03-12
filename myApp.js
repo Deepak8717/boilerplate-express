07dd04577032f1c18221366580c24fc149f46821
Deepak Pundir <sendit2deepak@gmail.com>
2022-03-12 09:43:37 +0530

    first commit

--- myApp.js
+++ myApp.js
@@ -1,39 +1,5 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
+console.log("Hello World");
